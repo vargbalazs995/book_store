efeba5208d93c60f4d72ec84e7f4a4d49fdd0116
theotar <theotar995@gmail.com>
2024-07-05 13:46:06 +0200

    create relations between entities

--- backend/src/entities/bookEntity.ts
+++ backend/src/entities/bookEntity.ts
@@ -6,6 +6,7 @@ const bookSchema = new Schema({
     title: {type:String, required:true, unique: true},
     description: String,
     author: String,
+    reviews: {type:Schema.Types.ObjectId, ref:"Review"},
 })
 
 export const BookModel = mongoose.model('Books', bookSchema);
--- backend/src/entities/reviewEntity.ts
+++ backend/src/entities/reviewEntity.ts
@@ -5,6 +5,7 @@ const {Schema} = mongoose;
 const reviewSchema = new Schema({
     review: {type: String, min:50},
     rating: Number,
+    user: {type: Schema.Types.ObjectId, ref: "User"}
 })
 
 export const reviewModel = mongoose.model('Review', reviewSchema);
