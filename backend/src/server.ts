a944895a2c1cb4b688cf98d767f283bdd1573e71
theotar <theotar995@gmail.com>
2024-07-06 16:37:50 +0200

    fix module imports, add error logging for database connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,18 @@
 import {MONGODB_URI, ORIGIN, PORT} from "./configuration";
 import {rootRouter} from "./routes/rootRouter";
+import express from "express";
+import cors from "cors";
+import * as mongoose from "mongoose";
 
-const express = require('express');
-const cors = require('cors');
 const app = express();
-const mongoose = require('mongoose')
-
 app.use(express.json());
 app.use(cors({origin: ORIGIN, credentials: true}));
-mongoose.connect(MONGODB_URI, {}).then(() => {
-    app.use(rootRouter)
-    console.log('Connected to DB')
-})
+app.use(rootRouter);
+
+mongoose.connect(MONGODB_URI, {})
+    .then(() => { console.log(`Database successfully connected`); })
+    .catch((err) => console.error(`Error occurs until database connection: \n\r ${err}`))
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
