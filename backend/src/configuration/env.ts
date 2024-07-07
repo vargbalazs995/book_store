b78b0f747d35dc812565476a928f7b10b5c579c2
theotar <theotar995@gmail.com>
2024-07-07 21:11:39 +0200

    extend .env, export JWT_SECRET and SALT_ROUNDS env vars

--- backend/src/configuration/env.ts
+++ backend/src/configuration/env.ts
@@ -3,3 +3,5 @@ import "dotenv/config";
 export const PORT = parseInt(process.env.PORT ?? "3003");
 export const MONGODB_URI = process.env.MONGODB_URI ?? "mongodb://localhost";
 export const ORIGIN = process.env.ORIGIN ?? "http://localhost:3000/";
+export const JWT_SECRET = process.env.JWT_SECRET ?? "AIUSDHGAUISHD";
+export const SALT_ROUNDS = process.env.SALT_ROUNDS ?? 10;
