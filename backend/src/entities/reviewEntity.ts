57daf26f8e558f0fb6b520374793788af8126000
theotar <theotar995@gmail.com>
2024-07-07 20:46:15 +0200

    extend reviewSchema with book_id ref and set review and rating schema validation to be required

--- backend/src/entities/reviewEntity.ts
+++ backend/src/entities/reviewEntity.ts
@@ -4,14 +4,16 @@ export interface IReview extends Document{
     _id: Types.ObjectId,
     review: string,
     rating: number,
-    user: [Types.ObjectId]
+    book_id: Types.ObjectId,
+    user: Types.ObjectId
 }
 
 export const reviewSchema = new Schema<IReview>({
     _id: Schema.Types.ObjectId,
-    review: {type: String, min:50},
-    rating: Number,
-    user: [{type: Types.ObjectId, ref: "User"},]
+    review: {type: String, min:20,required: true},
+    rating: {type: Number, required:true},
+    book_id: {type: Schema.Types.ObjectId, ref: "Book"},
+    user: {type: Schema.Types.ObjectId, ref: "User"}
 })
 
 export const ReviewModel:Model<IReview> = model('Review', reviewSchema);
