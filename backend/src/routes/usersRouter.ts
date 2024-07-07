c48804e077f43ab82cc8b3bd7f98cf44cbd1262f
theotar <theotar995@gmail.com>
2024-07-07 21:14:51 +0200

    refactor usersService.ts define login and register namings

--- backend/src/routes/usersRouter.ts
+++ backend/src/routes/usersRouter.ts
@@ -1,38 +1,30 @@
 import {Router} from "express";
-import {CreateAccountDTO, LoginUserDTO} from "../dtos";
-import {createAccount, loginUser} from "../services/userService";
-import {validateOrReject} from "class-validator";
+import {LoginDTO, RegisterDTO} from "../dtos";
+import {register, login} from "../services/usersService";
+import {validationMiddleware} from "../middlewares/validationMiddleware";
 
 
 export const usersRouter = Router();
 
-usersRouter.post("", async (req, res,next) => {
-const registerUser: CreateAccountDTO = req.body;
-
+usersRouter.post("/register", validationMiddleware(RegisterDTO), async (req, res, next) => {
   try {
-      await validateOrReject(registerUser);
-      const message = await createAccount(registerUser);
+      const registerDTO: RegisterDTO = req.body;
+      const message = await register(registerDTO);
       res.status(201).json({message});
   }catch (error){
       return res.status(400).json({error: error});
   }
-})
-usersRouter.post("/login", async (req, res,next) => {
-    const loginUserDTO: LoginUserDTO = req.body;
-    try{
-        await validateOrReject(loginUserDTO)
+});
 
-    }catch (error){
-        next(error)
-    }
-
+usersRouter.post("/login", validationMiddleware(LoginDTO), async (req, res, next) => {
     try{
-        const token: string = await loginUser(loginUserDTO);
+        const LoginDTO: LoginDTO = req.body;
+        const token: string = await login(LoginDTO);
         res.status(200).json({ token });
     }catch (error){
         next(error)
         res.status(500).json({ error: error });
     }
-})
+});
 
 usersRouter.post("/me", async (req, res,next) => {})
---
+++ backend/src/services/usersService.ts
@@ -0,0 +1,39 @@
+import {RegisterDTO, LoginDTO} from "../dtos";
+import {UserModel} from "../entities/userEntity";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+import {JWT_SECRET, SALT_ROUNDS} from "../configuration";
+import {BadRequestError} from "../errors";
+
+export const register = async (registerDTO: RegisterDTO) => {
+    const registerUserDto = registerDTO
+    const checkUser = await UserModel.findOne({username: registerDTO.username})
+
+    if (checkUser) {
+        throw new BadRequestError('Username already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(registerUserDto.password, SALT_ROUNDS);
+
+    const newUser = new UserModel({
+        username: registerDTO.username,
+        password: hashedPassword,
+        email: registerDTO.email,
+    });
+    await newUser.save();
+    return jwt.sign({ id: newUser._id, username: newUser.username }, JWT_SECRET, { expiresIn: '1h' });
+}
+
+export const login= async(loginDTO:LoginDTO)=>{
+    const user = await UserModel.findOne({ username: loginDTO.username });
+
+    if (!user) {
+        return "Username doesn't exist"
+    }
+
+    const passwordMatch = await bcrypt.compare(loginDTO.password, user.password);
+    if (!passwordMatch) {
+        return "The password doesn't match";
+    }
+    return jwt.sign({ id: user._id, username: user.username }, JWT_SECRET, { expiresIn: '1h' });
+}
