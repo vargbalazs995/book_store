dbaaadd9df95bc286ee7728408e0e74fb2b16c05
theotar <theotar995@gmail.com>
2024-07-08 20:27:05 +0200

    create api call

--- frontend/src/app/books/book.service.ts
+++ frontend/src/app/books/book.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {BookDetails} from "./book.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+  private apiUrl:string = 'http://localhost:3000/books';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchAllBook(){
+    return this.http.get<BookDetails[]>(this.apiUrl)
+  }
+
+  getBookById(id: string) {
+    return this.http.get<BookDetails>(this.apiUrl+`/${id}`)
+  }
+
+  deleteBookById(id: string) {
+    return this.http.delete<BookDetails>(this.apiUrl+`/${id}`)
+  }
 }
