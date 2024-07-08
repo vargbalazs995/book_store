8283a68d585df96b9ff65c57a1a710f59edc5ddb
theotar <theotar995@gmail.com>
2024-07-08 02:22:44 +0200

    create authMiddleware.ts

---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../configuration';
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Token invalid' });
+        }
+
+        // @ts-ignore
+        req.userId = (decoded as any).id;
+        next();
+    });
+};
